8e4f3d1fb90ba3a20ef212586fcb1fa61aa74408
Benjamin Wolf <benjamin.wolf@eledia.de>
2017-03-14 17:34:34 +0100

    local_eledia_webservicesuite - add user idnumber to return params in get_grade_by_timespan function

--- local/eledia_webservicesuite/externallib.php
+++ local/eledia_webservicesuite/externallib.php
@@ -1694,7 +1694,7 @@ class eledia_services extends external_api {
         $params = self::validate_parameters(self::get_grade_by_timespan_parameters(), $params);
 
         $sql_params = array($params['grade']['from'], $params['grade']['to']);
-        $sql = 'SELECT gg.id, u.username, gg.userid, c.fullname AS coursename, gi.courseid, gg.finalgrade, gi.itemname, gi.itemtype, gi.grademax
+        $sql = 'SELECT gg.id, u.username, u.idnumber, gg.userid, c.fullname AS coursename, gi.courseid, gg.finalgrade, gi.itemname, gi.itemtype, gi.grademax
                 FROM {grade_grades} gg, {grade_items} gi, {user} u, {course} c
                 WHERE gg.timemodified > ?
                 AND gg.timemodified < ?
@@ -1745,6 +1745,8 @@ class eledia_services extends external_api {
                                     new external_value(PARAM_RAW, 'The final grade the user gets in the modul/course.'),
                                 'percent' =>
                                     new external_value(PARAM_RAW, 'The final grade as percent of possible points.'),
+                                'idnumber' =>
+                                    new external_value(PARAM_RAW, 'Idnumber filed of the user..', VALUE_OPTIONAL),
                                 'itemname' =>
                                     new external_value(PARAM_RAW, 'If it is a module this contains the modul instance name.', VALUE_OPTIONAL),
                                 'itemtype' =>
